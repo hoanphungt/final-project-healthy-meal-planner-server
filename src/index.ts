f98c09e22551b58f66eb5771469678508fd39447
Alina <alex.alina.rusu@gmail.com>
2018-12-11 16:16:06 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import 'reflect-metadata'
+import { /*Action, BadRequestError,*/ createKoaServer } from "routing-controllers"
+import setupDb from './db'
+// import UserController from './users/controller';
+// import { verify } from './jwt'
+// import User from './users/entity'
+// import LoginController from './logins/controller';
+
+const port = process.env.PORT || 4000
+
+const app = createKoaServer({
+  cors: true,
+  controllers: [
+    // UserController,
+    // LoginController,
+  ],
+  /*
+  authorizationChecker: (action: Action) => {
+    const header: string = action.request.headers.authorization
+    if (header && header.startsWith('Bearer ')) {
+      const [, token] = header.split(' ')
+
+      try {
+        return !!(token && verify(token)) 
+      }
+      catch (e) {
+        throw new BadRequestError(e)
+      }
+    }
+
+    return false
+  },
+
+  currentUserChecker: async (action: Action) => {
+    const header: string = action.request.headers.authorization
+    if (header && header.startsWith('Bearer ')) {
+      const [, token] = header.split(' ')
+
+      if (token) {
+        const { id } = verify(token) 
+        return User.findOne(id)
+      }
+    }
+    return undefined
+  },
+
+  */
+})
+
+setupDb()
+  .then(_ =>
+    app.listen(port, () => console.log(`Listening on port ${port}`))
+  )
+  .catch(err => console.error(err))
+
